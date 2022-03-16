c256a77f76e700148af4988ec3f4600557c436eb
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-03-16 19:28:20 +0700

    fix image logo

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -2,7 +2,8 @@
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
      <!-- Brand Logo -->
      <a href="/admin" class="brand-link">
-         <img src="/img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+         <img src="{{ asset('img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+             style="opacity: .8">
          <span class="brand-text font-weight-light">Adi Karya</span>
      </a>
 
